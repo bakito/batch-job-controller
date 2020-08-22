693d61d8462abbbf53a106862715598308095dfe
bakito <github@bakito.ch>
2020-08-22 23:22:31 +0200

    validation test

--- pkg/http/types_test.go
+++ pkg/http/types_test.go
@@ -3,6 +3,7 @@ package http_test
 import (
 	"github.com/bakito/batch-job-controller/pkg/http"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("types", func() {
@@ -10,11 +11,55 @@ var _ = Describe("types", func() {
 		event *http.Event
 	)
 	BeforeEach(func() {
-		event = &http.Event{}
+		event = &http.Event{
+			Eventtype: "Normal",
+			Reason:    "AnyReason",
+			Message:   "message",
+		}
 	})
 	Context("Event.Validate", func() {
-		It("shoud be valid", func() {
-			event.Validate()
+		It("should be valid", func() {
+			err := event.Validate()
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+		It("should fail with an invalid eventtype", func() {
+			event.Eventtype = "foo"
+			err := event.Validate()
+			Ω(err).Should(HaveOccurred())
+		})
+		It("should fail with an empty eventtype", func() {
+			event.Eventtype = ""
+			err := event.Validate()
+			Ω(err).Should(HaveOccurred())
+		})
+		It("should fail with an empty reason", func() {
+			event.Reason = ""
+			err := event.Validate()
+			Ω(err).Should(HaveOccurred())
+		})
+		It("should fail with an lowercase reason", func() {
+			event.Reason = "reason"
+			err := event.Validate()
+			Ω(err).Should(HaveOccurred())
+		})
+		It("should fail with with both messages empty", func() {
+			event.Message = ""
+			event.MessageFmt = ""
+			err := event.Validate()
+			Ω(err).Should(HaveOccurred())
+		})
+		It("should be valid with messages", func() {
+			event.Message = "foo"
+			event.MessageFmt = ""
+			err := event.Validate()
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+		It("should be valid with messagesFmt", func() {
+			event.Message = ""
+			event.MessageFmt = "foo %s"
+			event.Args = []string{"bar"}
+			err := event.Validate()
+			Ω(err).ShouldNot(HaveOccurred())
 		})
 	})
 })
