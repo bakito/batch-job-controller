c625d3dc241bbaeb6d823a17fdf80211e78cb9c1
bakito <github@bakito.ch>
2026-03-04 22:22:15 +0100

    check for erro in log

--- testdata/e2e/checkAndPrintControllerLogsAndEvents.sh
+++ testdata/e2e/checkAndPrintControllerLogsAndEvents.sh
@@ -5,6 +5,17 @@ LABEL_SELECTOR='app=example-job-controller'
 
 NAMESPACE=e2e-batch
 
+echo "---- events (namespace/${NAMESPACE}) ----"
+events="$(kubectl get events -n "${NAMESPACE}" --sort-by='.lastTimestamp')"
+echo "${events}"
+
+if [[ -n "${GITHUB_STEP_SUMMARY:-}" ]]; then
+  echo 'Namespace Events' >> $GITHUB_STEP_SUMMARY
+  echo '```' >> $GITHUB_STEP_SUMMARY
+  echo "${events}" >> $GITHUB_STEP_SUMMARY
+  echo '```' >> $GITHUB_STEP_SUMMARY
+fi
+
 pod="$(kubectl get pod -n "${NAMESPACE}" -l "${LABEL_SELECTOR}" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')"
 
 echo "---- logs (pod/${pod}) ----"
@@ -18,14 +29,7 @@ if [[ -n "${GITHUB_STEP_SUMMARY:-}" ]]; then
   echo '```' >> $GITHUB_STEP_SUMMARY
 fi
 
-echo "---- events (namespace/${NAMESPACE}) ----"
-events="$(kubectl get events -n "${NAMESPACE}" --sort-by='.lastTimestamp')"
-echo "${events}"
-
-if [[ -n "${GITHUB_STEP_SUMMARY:-}" ]]; then
-  echo 'Namespace Events' >> $GITHUB_STEP_SUMMARY
-  echo '```' >> $GITHUB_STEP_SUMMARY
-  echo "${events}" >> $GITHUB_STEP_SUMMARY
-  echo '```' >> $GITHUB_STEP_SUMMARY
+if echo "${logs}" | grep -q '"level":"error"'; then
+  echo "💥 ERROR: Found error level logs in controller"
+  exit 1
 fi
-
